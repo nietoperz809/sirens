14844444fff487fda4c3cc3a11ef219f0998e63d
Administrator <pbull809@gmail.com>
2020-09-16 07:24:35 +0200

    Initial commit

---
+++ src/sirens/ClipHandler.java
@@ -0,0 +1,50 @@
+package sirens;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.Line;
+import java.io.InputStream;
+import java.util.HashMap;
+
+public class ClipHandler
+{
+    private static HashMap<String, Clip> map = new HashMap<>();
+
+    private static Clip playWave (InputStream is) throws Exception
+    {
+        final Clip clip = (Clip) AudioSystem.getLine (new Line.Info (Clip.class));
+        clip.open (AudioSystem.getAudioInputStream (is));
+        clip.loop (Clip.LOOP_CONTINUOUSLY);
+        clip.start ();
+        return clip;
+    }
+
+    private static void stopWave (Clip clip)
+    {
+        clip.stop ();
+        clip.close ();
+    }
+
+    public static void startStop (String name)
+    {
+        Clip cl = map.get (name);
+        if (cl != null)
+        {
+            cl.stop();
+            cl.close();
+            map.remove (name);
+        }
+        else
+        {
+            InputStream is = ClassLoader.getSystemResourceAsStream (name);
+            try
+            {
+                cl = playWave (is);
+                map.put (name, cl);
+            } catch (Exception e)
+            {
+                System.out.println (e);
+            }
+        }
+    }
+}
