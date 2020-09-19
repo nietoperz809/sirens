55de6bfbe121ee0afb54f1f8e66316cdd15d93f2
Administrator <pbull809@gmail.com>
2020-09-19 03:02:14 +0200

    catch null ptr

--- src/sirens/Utils.java
+++ src/sirens/Utils.java
@@ -2,13 +2,17 @@ package sirens;
 
 import java.io.BufferedInputStream;
 import java.io.InputStream;
-import java.util.Objects;
 
 public class Utils
 {
     public static InputStream getRessource (String name)
     {
         InputStream is = ClassLoader.getSystemResourceAsStream (name);
+        if (is == null)
+        {
+            System.out.println ("could not load: "+name);
+            return null;
+        }
         return new BufferedInputStream (is);
     }
 }
